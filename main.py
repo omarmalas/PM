1f00f13729d3b4f89a1f0afc98de8ca63af9ca36
Omar Malas <63895256+omarmalas@users.noreply.github.com>
2023-11-23 07:08:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,21 +13,22 @@ def index():
 
 @app.route('/login.html')
 def login():
-    print("welco")
+    return render_template('login.html')
 
-@app.route('/generate_password', methods=['POST'])
 def generate_password(length=12):
     characters = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(secrets.choice(characters) for _ in range(length))
+    return password
 
-    if __name__ == "__main__":
-        try:
-            password_length = int(request.form.get('password_length', 12))
-            generated_password = generate_password(password_length)
-            return render_template('index.html', generated_password=generated_password)
-        except ValueError:
-            return render_template('index.html',
-                                   error="Invalid input. Please enter a valid number for password length.")
+@app.route('/generate_password', methods=['POST'])
+def generate_password_route():
+    try:
+        password_length = int(request.form.get('password_length', 12))
+        generated_password = generate_password(password_length)
+        return render_template('index.html', generated_password=generated_password)
+    except ValueError:
+        return render_template('index.html', error="Invalid input. Please enter a valid number for password length.")
+
 
 @app.route('/check_strength', methods=['POST'])
 def check_strength():
@@ -39,4 +40,3 @@ def check_strength():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
