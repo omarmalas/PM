6ec41ed9db0b6cfe902fc431c0afa5a0bdba80df
Omar Malas <63895256+omarmalas@users.noreply.github.com>
2023-12-18 12:04:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,79 @@
-from flask import Flask, render_template, request
-import secrets #used for generating cryptographically strong random numbers suitable for managing data such as passwords, account authentication, security tokens, and related secrets.
+from flask import Flask, render_template, request, redirect, url_for
+import secrets
+# used for generating cryptographically strong random numbers suitable for managing data such as passwords,
+# account authentication, security tokens, and related secrets.
 import string
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_bcrypt import check_password_hash
 
 app = Flask(__name__)
-#https://github.com/yanalabuseini/hexxus/tree/main
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
+
+# https://github.com/yanalabuseini/hexxus/tree/main
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/login.html')
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        try:
+            username = request.form['username']
+            plain_password = request.form['password']
+
+            # Hash the password before storing it
+            hashed_password = bcrypt.generate_password_hash(plain_password).decode('utf-8')
+
+            new_user = User(username=username, password=hashed_password)
+            db.session.add(new_user)
+            db.session.commit()
+
+            return redirect(url_for('index'))
+        except KeyError:
+            return render_template('register.html', error='Invalid form data.')
+
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        try:
+            username = request.form['username']
+            plain_password = request.form['password']
+
+            user = User.query.filter_by(username=username).first()
+
+            if user and check_password_hash(user.password, plain_password):
+                # Password matches
+                return redirect(url_for('index'))
+            else:
+                return render_template('login.html', error='Invalid username or password.')
+        except KeyError:
+            return render_template('login.html', error='Invalid form data.')
+
     return render_template('login.html')
+
 
 def generate_password(length=12):
     characters = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(secrets.choice(characters) for _ in range(length))
     return password
+
 
 @app.route('/generate_password', methods=['POST'])
 def generate_password_route():
